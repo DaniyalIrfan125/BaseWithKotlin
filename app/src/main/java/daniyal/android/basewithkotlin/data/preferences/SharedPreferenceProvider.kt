432041afc1cb2f3f5f65e8845dbe899dc02946e1
Danial <daniyalirfan.itdev@gmail.com>
2019-12-03 06:38:29 -0800

    Constants class added

--- app/src/main/java/daniyal/android/basewithkotlin/data/preferences/SharedPreferenceProvider.kt
+++ app/src/main/java/daniyal/android/basewithkotlin/data/preferences/SharedPreferenceProvider.kt
@@ -2,22 +2,23 @@ package daniyal.android.basewithkotlin.data.preferences
 
 import android.content.Context
 import android.content.SharedPreferences
+import daniyal.android.basewithkotlin.utils.AppConstants
 
 class SharedPreferenceProvider(context: Context) {
 
     private val appContext = context.applicationContext
     private val sharedPreference :SharedPreferences
-    get()= appContext.getSharedPreferences("MyAppPreferences",0)
+    get()= appContext.getSharedPreferences(AppConstants.SHAREDPREFERENCENAME,0)
 
 
     fun insertData (item :String){
 
-        sharedPreference.edit().putString("mykey",item).apply()
+        sharedPreference.edit().putString(AppConstants.keyInsertData,item).apply()
     }
 
     fun getData ():String?{
 
-        return sharedPreference.getString("mykey",null)
+        return sharedPreference.getString(AppConstants.keyInsertData,null)
     }
 
 }
---
+++ app/src/main/java/daniyal/android/basewithkotlin/utils/AppConstants.kt
@@ -0,0 +1,8 @@
+package daniyal.android.basewithkotlin.utils
+
+object AppConstants {
+
+    const val SHAREDPREFERENCENAME = "MyAppSharedPreferences"
+
+    const val keyInsertData = "MyTestingKey"
+}
